19793e3c7f8422692211b4a80caacd20e35569a6
VijayKrishna32 <vijji143258@gmail.com>
2021-04-22 22:39:00 +0530

    Second Commit

--- src/main/java/com/zensar/sampleappweb/controller/HelloController.java
+++ src/main/java/com/zensar/sampleappweb/controller/HelloController.java
@@ -10,4 +10,14 @@ public class HelloController {
 	public String sayHello() {
 		return "<h1>Welcome TO Jenkins.</h1>";
 	}
+	
+	@RequestMapping("/hi")
+	public String sayHi() {
+		return "<h1>Hi User</h1>";
+	}
+	
+	@RequestMapping("/Bye")
+	public String sayBye() {
+		return "<h1>Good Bye/h1>";
+	}
 }
